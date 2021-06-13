950528edb5f8d3c1957ff468732e52a24b11ff8a
hideki1104 <hideki11047@gmail.com>
2021-06-13 16:33:18 +0900

    新規登録フォームにバリデーションを追加

--- src/Components/Auth/RegistrationForm.tsx
+++ src/Components/Auth/RegistrationForm.tsx
@@ -19,23 +19,30 @@ const useStyles = makeStyles((theme: Theme) =>
     title: {
       fontSize: 30,
       fontWeight: 'bold',
-      paddingTop: 30,
+      paddingTop: 20,
     },
 
     authForm: {
       width: 300,
-      marginTop: 30,
+      marginTop: 20,
+      textAlign: "center",
     },
 
     authButton: {
       fontSize: 16,
       width: 300,
       height: 50,
-      marginTop: 30,
+      marginTop: 20,
     },
     repletion: {
       color: "rgba(0, 0, 0, 0.54)",
     },
+
+    error_message: {
+      height: 10,
+      color: "red",
+      fontSize: 12,
+    }
   }),
 );
 
@@ -64,21 +71,38 @@ export const RegistrationForm: React.FC<RegistrationFormProps> = () => {
       <Typography className={classes.title} color="textSecondary" gutterBottom>
         新規登録
       </Typography>
-      <CardContent>
         <form className={classes.root} onSubmit={handleSubmit(handleOnSubmit)}>
           <TextField className={classes.authForm} id="name" label="お名前" variant="outlined" type="text" {...register("name", { required: true, maxLength: 50 })}/><br/>
+          <span className={classes.error_message}>
+            {errors.name && errors.name.type === "required" && "お名前を入力してください"}
+            {errors.name && errors.name.type === "maxLength" && "50文字以内で入力してください"}
+          </span><br/>
 
-          <TextField className={classes.authForm} id="email" label="メールアドレス" variant="outlined" type="email" {...register("email", { required: true })}/><br/>
+          <TextField className={classes.authForm} id="email" label="メールアドレス" variant="outlined" type="email" {...register("email", { required: true,
+            pattern: {
+            value: /\S+@\S+\.\S+/,
+            message: "メールアドレスの形式が違います" }})}/><br/>
+          <span className={classes.error_message}>
+            {errors.email && errors.email.type === "required" && "メールアドレスを入力してください"}
+            {errors.email && errors.email.type === "pattern" && "メールアドレスの形式が違います"}
+          </span><br/>
 
-          <TextField className={classes.authForm} id="password" label="パスワード" variant="outlined" type="password" {...register("password", { required: true })}/><br/>
+          <TextField className={classes.authForm} id="password" label="パスワード" variant="outlined" type="password" {...register("password", { required: true, minLength: 6 })}/><br/>
+          <span className={classes.error_message}>
+            {errors.password && errors.password.type === "required" && "パスワードを入力してください"}
+            {errors.password && errors.password.type === "minLength" && "パスワードは6文字以上で入力してください"}
+          </span><br/>
 
-          <TextField className={classes.authForm} id="password_comfirmation" label="パスワード(確認用)" variant="outlined" type="password" {...register("password_confirmation", { required: true })}/><br/>
+          <TextField className={classes.authForm} id="password_confirmation" label="パスワード(確認用)" variant="outlined" type="password" {...register("password_confirmation", { required: true, minLength: 6 })}/><br/>
+          <span className={classes.error_message}>
+            {errors.password_confirmation && errors.password_confirmation.type === "required" && "パスワード(確認用)を入力してください"}
+            {errors.password_confirmation && errors.password_confirmation.type === "minLength" && "パスワード(確認用)は6文字以上で入力してください"}
+          </span><br/>
 
           <Button className={classes.authButton} variant="contained" type="submit">登録</Button>
 
           <p className={classes.repletion}>すでにアカウントをお持ちの方は<Link to="/sign_in">こちら</Link></p>
         </form>
-      </CardContent>
     </Card>
   );
 }
