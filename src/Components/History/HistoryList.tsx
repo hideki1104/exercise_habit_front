7d27b0d51ef9293e2c920d662305acb7f734298f
hideki1104 <hideki11047@gmail.com>
2021-09-15 20:49:55 +0900

    履歴表示機能を追加

--- src/Components/History/HistoryList.tsx
+++ src/Components/History/HistoryList.tsx
@@ -1,42 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { connectGet } from '../Api/ConnectApi';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import ImageIcon from '@material-ui/icons/Image';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    movieContainer: {
-      margin: 20,
-      '&:hover': {
-        opacity: 0.6,
-      }
-    },
-    movie: {
-      height: 180,
-      width: "100%",
-      backgroundColor: "#F5F5F5",
-    },
-    difficulyType: {
-      fontSize: 12,
-      color: "#808080",
-    },
-    yt_thumnail: {
-      height: 180,
-      width: "100%",
-    },
-    paper: {
-      position: 'absolute',
-      width: 1100,
+    root: {
+      width: '100%',
       backgroundColor: theme.palette.background.paper,
-      boxShadow: theme.shadows[5],
-      padding: theme.spacing(2, 4, 3),
-      textAlign: "left",
     },
-    edit_link: {
-      textDecoration: "none",
-      color: "#ffffff",
-    },
-    genreSearch: {
-      width: 200,
+    history_img: {
+      width: 50,
+      height: 50,
     }
   }),
 );
@@ -45,11 +25,32 @@ interface HistoryListProps {
 }
 
 export const HistoryList: React.FC<HistoryListProps> = () => {
+  const classes = useStyles();
+
+  const convertJst = (date:string) => {
+    const createdAt = new Date(date)
+    createdAt.setTime(createdAt.getTime() + 9)
+    return createdAt.toLocaleString('ja-JP').slice(0,-3);
+  }
+
+  type HistoryData = {
+    id: number
+    name: string
+    set_count: number
+    url: string
+    thumbnail_id: string
+    created_at: string
+  }
+
+  const [historiesList, setHistoriesList] = useState<HistoryData[]>([]);
 
   useEffect(() => {
     const connectGetHistoryList = async () => {
       const responseData = await connectGet(`http://localhost:3000/histories`);
-      console.log(responseData);
+      if (!responseData.isSuccess) {
+        return;
+      }
+      setHistoriesList(responseData.data);
     }
 
     connectGetHistoryList();
@@ -57,6 +58,16 @@ export const HistoryList: React.FC<HistoryListProps> = () => {
 
   return (
     <>
+      <List className={classes.root}>
+        {historiesList.map((history) => (
+          <ListItem>
+            <ListItemAvatar>
+              <img className={classes.history_img} id="img" alt="" src={`https://i.ytimg.com/vi/${history ? history.url : ""}/${history ? history.thumbnail_id : ""}`}></img>
+            </ListItemAvatar>
+            <ListItemText primary={`${history ? history.name : ""} × ${history ? history.set_count : ""}セット`} secondary={`${history ? convertJst(history.created_at) : ""}`} />
+          </ListItem>
+        ))}
+      </List>
     </>
   );
 }
